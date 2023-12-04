4ee6601090257e22ca2cf6eda8303b772902a337
DanWaywell <34028193+DanWaywell@users.noreply.github.com>
2023-12-04 15:10:15 +0000

    Add moving platforms

--- src/character/ledge_grab_node.gd
+++ src/character/ledge_grab_node.gd
@@ -3,6 +3,8 @@ extends Node2D
 var on_ledge := false
 var can_turn := true
 
+var moving_platform: AnimatableBody2D = null
+
 @onready var character: Character = $".."
 @onready var ray_cast_down := $RayCastDown
 @onready var ray_cast_up := $RayCastUp
@@ -22,6 +24,10 @@ func _physics_process(_delta):
 	if not character.is_on_floor() and not on_ledge\
 	and ray_cast_down.is_colliding() and not ray_cast_up.is_colliding():
 		
+		var collider = ray_cast_down.get_collider()
+		if collider.is_class("AnimatableBody2D"):
+			moving_platform = collider
+		
 		on_ledge = true
 		can_turn = false
 		ray_cast_down.enabled = false
@@ -33,7 +39,8 @@ func _physics_process(_delta):
 		else:
 			character.position.x = ray_cast_forward.get_collision_point().x + 3
 			character.position.y = ray_cast_down.get_collision_point().y + 4
-	
+		
+		
 	# Stop ledge grab
 	if on_ledge == true:
 		
@@ -43,12 +50,22 @@ func _physics_process(_delta):
 			timer.start()
 			character.start()
 			character.jump()
+			moving_platform = null
 			
 		elif Input.is_action_just_pressed("down"):
 			on_ledge = false
 			can_turn = true
 			timer.start()
 			character.start()
+			moving_platform = null
+		
+		if moving_platform != null:
+			if scale.x == 1:
+				character.position.x = moving_platform.position.x - 11
+				character.position.y = moving_platform.position.y
+			else:
+				character.position.x = moving_platform.position.x + 11
+				character.position.y = moving_platform.position.y
 
 
 func _on_timer_timeout():
