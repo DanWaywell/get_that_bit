b94b982b913ab512243a2105ed83cf1fa5dd1984
DanWaywell <34028193+DanWaywell@users.noreply.github.com>
2024-08-08 11:55:49 +0100

    Merge game.gd and process.gd

--- arcade_game/game.gd
+++ arcade_game/game.gd
@@ -14,6 +14,27 @@ var level_number: int = FIRST_LEVEL_NUMBER
 var level_path : String = PATH_FIRST_LEVEL
 
 
+func _process(_delta):
+	# Quit
+	if Input.is_action_just_pressed("ui_cancel"):
+		if get_tree().current_scene.name == "TitleScreen":
+			if OS.get_name() == "Web":
+				return
+			else:
+				get_tree().quit()
+		else:
+			go_to_title_screen()
+	
+	# Fullscreen
+	if Input.is_action_just_pressed("fullscreen"):
+		if DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			DisplayServer.window_set_size(Vector2i(1280, 720))
+			DisplayServer.window_set_position(Vector2i(320, 180))
+
+
 func start():
 	_change_scene_to(level_path)
 
